a3f5835cdc1671556408fcd5cf4eb1f600376860
mikeinredding <46199522+mikeinredding@users.noreply.github.com>
2025-12-03 12:36:17 -0800

    continued work on edyduo.sh
    continued work to get automated install working

--- scripts/eddyduotest.sh
+++ scripts/eddyduotest.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-set -e
+set -x
 function eddy_message(){
   top_line
   title 'EddyDUo' "${yellow}"
   inner_line
   hr
-  echo -e " ¦ ${cyan}Installs Vsevolod-Volkov K1-Klipper-Eddy functionality        ${white}¦"
-  echo -e " ¦ ${cyan}fusing (Guilouz) Creality Helper Script as a framework                                               ${white}¦"
+  echo -e " ¦ ${cyan}Installs Vsevolod-Volkov K1-Klipper-Eddy              ${white}¦"
+  echo -e " ¦ ${cyan}functionality using (Guilouz) Creality Helper Script as a framework                                               ${white}¦"
   hr
   bottom_line
 }
@@ -15,43 +15,41 @@ function eddy_message(){
 function install_eddyduo(){
   eddy_message
   local yn
-	while true; do 
+	while true; do
     	install_msg "Install EddyDuo" yn
 		case "${yn}" in
       		Y|y)
         	echo -e "${white}"
-        	if [ -f "$EDDY_CFG"]; then
-          		rm -f "$EDDY_CFG" 
+        	if [ -f "$EDDYHS_CONFIG_FOLDER"/eddy/btteddy.cfg ]; then
+          		rm -f "$EDDYHS_CONFIG_FOLDER"/eddy/btteddy.cfg 
         	fi
         echo
-        	if [ "$model" = "K1" ]; then 
+        	if [ "$model" = "K1" ]; then
           		local printer_choice
-          		while true; do 
+          		while true; do
             		read -p " ${white}Do you want install it for ${yellow}K1${white} or ${yellow}K1 Max${white}? (${yellow}k1${white}/${yellow}k1max${white}): ${yellow}" printer_choice
             		case "${printer_choice}" in
               			K1|k1)
                 			echo -e "${white}"
                 			echo -e "Info: Copying file..."
                 			cp -f "$EDDY_K1_URL" "$EDDY_FOLDER"/eddy.cfg
-				 	cp -f "$EDDY_CONFIG/fan_control.cfg" "$EDDY_FOLDER"/fan_control.cfg
-				 	rsync --verbose --recursive $EDDY_KLIPPY $KLIPPER_KLIPPY_FOLDER
-
+				 			cp -f "$EDDY_CONFIG/fan_control.cfg" "$EDDY_FOLDER"/fan_control.cfg
+				 			rsync --verbose --recursive ./files/eddy/klippy/ /usr/share/klipper/klippy/
                		 	break;;
               			K1MAX|k1max)
                 			echo -e "${white}"
                 			echo -e "Info: Copying files..."
                 			cp -f "$BTTEDDY_K1M_URL" "$EDDY_FOLDER"/eddy.cfg
-					cp -f "$EDDY_CONFIG/fan_control.cfg" "$EDDY_FOLDER"/fan_control.cfg
-                			rsync --verbose --recursive $EDDY_KLIPPY $EDDY_KLIPPY $KLIPPER_KLIPPY_FOLDER
+							cp -f "$EDDY_CONFIG/fan_control.cfg" "$EDDY_FOLDER"/fan_control.cfg
+                			rsync --verbose --recursive ./files/eddy/klippy/ /usr/share/klipper/klippy/
                 		break;;
               			*)
                 			error_msg "Please select a correct choice!";;
 					esac
-		          done 
         	else
           		echo -e "Shouldn't get this..."
        		fi
-        
+       		fi
         if grep -q "include eddyhelper/eddy/eddy.cfg" "$PRINTER_CFG" ; then
           echo -e "Info: Eddy configurations are already enabled in printer.cfg file..."
         else
@@ -76,27 +74,25 @@ function install_eddyduo(){
 		  			G0028 {rawparams}
 		  			G90 ; Set to Absolute Positioning
 		  			G0 X{POSITION_X} Y{POSITION_Y} F3000 ; Move to bed center
-		  			{% if not rawparams or (rawparams and 'Z' in rawparams) %} #added when combineing eddy configfiles
+ 		  			{% if not rawparams or (rawparams and 'Z' in rawparams) %} #added when combineing eddy configfiles
     					PROBE #added when combineing eddy configfiles
     					SET_Z_FROM_PROBE #added when combineing eddy configfiles
   		  			{% endif %} #added when combineing eddy configfiles
-EOF
-#DO NOT ADD WHITESPACE OR TABS OR ANYTHING TO THE LINR WITHEOF
+			EOF
 	 		sed -i -e '/\[gcode_macro _IF_HOME_Z\]/!b' -e "r $TEMP_FILE" -e 'd' -e 'G' "$FILE_PATH"
 	 		rm "$TEMP_FILE"
-
         fi
-
+        done
         echo -e "Info: Restarting Klipper service..."
         restart_klipper
-        ok_msg "Eddy installed successfully....I hope!"
+        ok_msg "BTTEddy installed successfully....I hope!"
         return;;
       N|n)
         error_msg "Installation canceled!"
-        return;; # Exit the function if canceled
-
+        return;;
       *)
         error_msg "Please select a correct choice!";;
     esac
   done
+}
 }
